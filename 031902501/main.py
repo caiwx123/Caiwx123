c03d7e121cbc879cc7236e50a6d4d2ffb85753cc
caiwx123 <90031582+caiwx123@users.noreply.github.com>
2021-09-16 19:55:24 +0800

    Update main.py
    最终

--- 031902501/main.py
+++ 031902501/main.py
@@ -1,3 +1,4 @@
+
 import pypinyin
 import sys
 
@@ -16,12 +17,34 @@ class AC(object):
     # 以AC自动机来解决垃圾邮件过滤
 
     def __init__(self, words):
+        # line:行号列表 lj_in_txt:文中垃圾词列表  lj_words: 垃圾词原型的列表 count:已发现几个敏感词
+        self.line = []
+        self.lj_in_txt = []
+        self.lj_words = []
+        self.count = 0
+        self.pinyin = dict()
         self.root = AC.ac_build(self, words)
+
+    def c_pinyin(self, c):
+        if c in self.pinyin.keys():
+            c = self.pinyin[c]
+        else:
+            s = c
+            for x in pypinyin.pinyin(c, style=pypinyin.NORMAL):
+                c = ''.join(x)
+            self.pinyin[s] = c
+        return c
 
     def ac_build(self, f):
         # 建立AC自动机
-        with open(f, encoding='UTF-8') as file_object:
-            words = file_object.read().split('\n')
+        words = ''
+        try:
+            with open(f, 'r', encoding='UTF-8') as file_object:
+                words = file_object.read().split('\n')
+        except FileNotFoundError:
+            print("无法打开指定文件")
+        except UnicodeError:
+            print("读文件时解码错误")
         root = Node()
         # 树建立 key为拼音
         for word in words:
@@ -31,8 +54,7 @@ class AC(object):
             for c in word:
                 # 判断c是否为中文 是中文则改成拼音
                 if '\u4e00' <= c <= '\u9fff':
-                    for x in pypinyin.pinyin(c, style=pypinyin.NORMAL):
-                        c = ''.join(x)
+                    c=self.c_pinyin(c)
                 else:
                     # 将字母小写化
                     c = c.lower()
@@ -46,7 +68,6 @@ class AC(object):
                     if len(c) > 1:
                         # 全拼音首字母
                         son.append(Node(c[0]))
-
                     # 11111111111111 加个识别这个汉字可否拆分
 
                     # 连接父亲结点与儿子结点
@@ -66,7 +87,6 @@ class AC(object):
                 p = son.copy()
             for i in range(0, len(p)):
                 p[i].word = word
-
         # fail 指针的连接
         q = []
         q.insert(0, root)
@@ -87,15 +107,17 @@ class AC(object):
         root.fail = root
         return root
 
-    def search(self, f, out_file):
+    def search(self, f):
         # 从f中寻找敏感词汇并写入out_file
-        with open(f, encoding='UTF-8') as file_object:
-            t = file_object.read().split('\n')
-        count = 0
-        # line:行号列表 lj_in_txt:文中垃圾词列表  lj_words: 垃圾词原型的列表
-        line = []
-        lj_in_txt = []
-        lj_words = []
+        t = []
+        try:
+            with open(f, 'r', encoding='UTF-8') as file_object:
+                t = file_object.read().split('\n')
+        except FileNotFoundError:
+            print("无法打开指定文件")
+        except UnicodeError:
+            print("读文件时解码错误")
+
         for j in range(0, len(t)):
             # txt:原文的第J行 tg:文中垃圾词中最后出现的连续的无意字符个数 begin:垃圾词的开头index
             txt = t[j]
@@ -114,8 +136,7 @@ class AC(object):
                         begin = i
                         tg = 0
                     # 将中文转成拼音
-                    for x in pypinyin.pinyin(c, style=pypinyin.NORMAL):
-                        c = ''.join(x)
+                    c=self.c_pinyin(c)
                 elif 'a' <= c <= 'z' or 'A' <= c <= 'Z':
                     c = c.lower()
                 # 此词为无意词 进行 相应操作并跳过
@@ -124,10 +145,7 @@ class AC(object):
                     continue
                 else:
                     if p.word:
-                        line.append(j)
-                        lj_words.append(p.word)
-                        lj_in_txt.append(txt[begin:i - tg])  # 第J行的从begin到 i-tg
-                        count += 1
+                        self.put_in(j, p.word, txt[begin:i - tg])
                     p = self.root
                     tg = 0
                     continue
@@ -139,10 +157,7 @@ class AC(object):
                         tg = 0
                         continue
                     if p.word:
-                        line.append(j)
-                        lj_words.append(p.word)
-                        lj_in_txt.append(txt[begin:i - tg])  # 第J行的从begin到 i-tg
-                        count += 1
+                        self.put_in(j, p.word, txt[begin:i - tg])
                         i -= 1
                         p = self.root
                         continue
@@ -150,22 +165,39 @@ class AC(object):
                     i -= 1
             # 解决由于垃圾词在结尾的问题
             if p.word:
-                line.append(j)
-                lj_words.append(p.word)
-                lj_in_txt.append(txt[begin:i + 1 - tg])
-                count += 1
-        # 输出
-        with open(out_file, 'w', encoding='UTF-8') as file_out:
-            file_out.write(f"Total : {count}")
-            file_out.write('\n')
-            for i in range(0, count):
-                file_out.write(f"Line{line[i] + 1}: <{lj_words[i]}> {lj_in_txt[i]}")
+                self.put_in(j, p.word, txt[begin:i + 1 - tg])
+
+    def put_in(self, j, word, word_in_txt):
+        # 将词入库
+
+        self.line.append(j)
+        self.lj_words.append(word)
+        self.lj_in_txt.append(word_in_txt)
+        self.count += 1
+
+    def out_f(self, out_file):
+        # 输出到文件中
+        try:
+            with open(out_file, 'w', encoding='UTF-8') as file_out:
+                file_out.write(f"Total : {self.count}")
                 file_out.write('\n')
+                for i in range(0, self.count):
+                    file_out.write(f"Line{self.line[i] + 1}: <{self.lj_words[i]}> {self.lj_in_txt[i]}")
+                    file_out.write('\n')
+        except FileNotFoundError:
+            print("无法打开指定文件")
+        except UnicodeError:
+            print("读文件时解码错误")
 
 
 if __name__ == '__main__':
-    words = sys.argv[1]
-    org = sys.argv[2]
-    ans = sys.argv[3]
-    ac = AC(words)
-    ac.search(org, ans)
+    if len(sys.argv) != 4:
+        print("Wrong number of parameters\nplease reenter")
+    else:
+        words = sys.argv[1]
+        org = sys.argv[2]
+        ans = sys.argv[3]
+        ac = AC(words)
+        ac.search(org)
+        ac.out_f(ans)
+
